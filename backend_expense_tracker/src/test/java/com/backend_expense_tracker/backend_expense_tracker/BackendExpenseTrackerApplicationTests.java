bc5199498ba7554172fcf62d8075ac88dae6a611
DuyL33 <lduyviet999@gmail.com>
2025-10-13 14:56:53 -0400

    feat: Adding spring backend directory

---
+++ backend_expense_tracker/src/test/java/com/backend_expense_tracker/backend_expense_tracker/BackendExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend_expense_tracker.backend_expense_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
